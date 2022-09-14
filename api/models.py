f2b295664fb920770b36004093bf0c35d2721d04
niyas <niyasmuhammed2325@gmail.com>
2022-09-14 14:46:09 +0530

    model relationship added

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,7 +7,15 @@ class Posts(models.Model):
     title=models.CharField(max_length=120)
     description=models.CharField(max_length=250)
     image=models.ImageField(upload_to="postimages",null=True)
-    user=models.CharField(max_length=120)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
     date=models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.title
+
+class Coments(models.Model):
+    post=models.ForeignKey(Posts,on_delete=models.CASCADE)
+    comment=models.CharField(max_length=200)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comment
