25561fe87c952cead58797b901aad7cfc12ecb9f
niyas <niyasmuhammed2325@gmail.com>
2022-09-22 19:23:16 +0530

    nothing

--- api/serializers.py
+++ api/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    password=serializers.CharField(write_only=True)      #do not read password
+    password=serializers.CharField(write_only=True)                         #do not read password
     class Meta:
         model=User
         fields=["first_name","last_name","email","username","password"]
